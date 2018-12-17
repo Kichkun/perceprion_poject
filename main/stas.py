bf3727490975ef2f453b0213fbc357b9c16fef5c
MrTsepa <tsepa.stas@gmail.com>
2018-12-17 22:49:40 +0300

    trajectory to odometry

--- main/stas.py
+++ main/stas.py
@@ -9,6 +9,10 @@ def wrap_angle(alpha):
     return alpha
 
 
+def project_traj(t):
+    return t[:, :2]
+
+
 def trajectory_to_odometry(trajectory):
     """
     :param trajectory: xs and ys
@@ -17,7 +21,7 @@ def trajectory_to_odometry(trajectory):
     motions: rot1, trans, rot2
     """
     xs = np.zeros((trajectory.shape[0], 3), dtype='float')
-    motions = np.zeros((trajectory.shape[0] - 1, 3), dtype='float')
+    motions = np.zeros((trajectory.shape[0], 3), dtype='float')
     xs[:, :2] = trajectory
     dx0 = trajectory[1, 0] - trajectory[0, 0]
     dy0 = trajectory[1, 1] - trajectory[0, 1]
@@ -30,5 +34,12 @@ def trajectory_to_odometry(trajectory):
         theta = xs[i - 1, 2]
         dtheta = wrap_angle(new_theta - theta)
         motions[i - 1, :] = [dtheta, dx ** 2 + dy ** 2, 0]
+    motions[-1] = motions[-2]
 
     return xs, motions
+
+
+if __name__ == '__main__':
+    trajectory = project_traj(np.load('trajectory.npy'))
+    path, motions = trajectory_to_odometry(trajectory)
+    np.savez('odometry', num_steps=500, noise_free_motion=motions, noise_free_robot_path=path)
